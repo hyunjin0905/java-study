ec8954862327277f1e09a8e088be8aab48146985
lucy <kkadangj@gmail.com>
2022-04-18 15:33:18 +0900

    [Feat] config-native

---
+++ MovieApplication.java
@@ -0,0 +1,9 @@
+public class MovieApplication {
+    public static void main (String[] args) {
+        Movie one = new Movie();
+        one.title = "주식과 함께 사라지다";
+        one.genre = "비극";
+        one.rating = -2;
+        one.playIt();
+    }
+}
