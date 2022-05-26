6d66fceab2275aa8baf6d5477ec2741f39f109ed
lucy <kkadangj@gmail.com>
2022-05-26 21:52:37 +0900

    [Fix]

--- MovieApplication.java
+++ MovieApplication.java
@@ -1,7 +1,7 @@
 public class MovieApplication {
     public static void main (String[] args) {
         Movie one = new Movie();
-        one.title = "주식과 함께 사라지다";
+        one.title = "주식과함께사라지다";
         one.genre = "비극";
         one.rating = -2;
         one.playIt();
