4141bfda51d2213284899509feed0e077d733c4c
lucy <kkadangj@gmail.com>
2022-06-03 23:04:11 +0900

    [Fix]

--- MovieApplication.java
+++ MovieApplication.java
@@ -1,7 +1,5 @@
 public class MovieApplication {
     public static void main (String[] args) {
         Movie one = new Movie();
-        one.title = "주식과함께사라지다";
-        one.genre = "비극";
     }
 }
